5caeba9d6e6952aabdfc08c6074e40d42ae0b51b
isazaaa <isazamanuel04@gmail.com>
2025-07-25 12:05:08 -0500

    Prueba para inactivar inventarios en carnes y fruver 11

--- routes/carnesyfruverRoute.js
+++ routes/carnesyfruverRoute.js
@@ -11,7 +11,6 @@ import {
   obtenerProductosZonaActiva,
   eliminarProductoCarnesYFruver,
   actualizarEstadoInventarioCarnesYFruver,
-  crearInventarioCarnesYFruver // Añadido por completitud
 } from '../controllers/CarnesYfruver.js';
 
 const router = express.Router();
@@ -21,8 +20,6 @@ const parseFormData = multer().none(); // Middleware para procesar FormData sin 
 // Endpoint para iniciar una zona en inventario_activoCarnesYfruver
 router.post('/iniciar-inventarioCarnesYfruver', parseFormData, iniciarZonaCarnesYFruver);
 
-// Endpoint para crear un nuevo inventario en inventario_carnesYfruver
-router.post('/crear-inventario-carnesYfruver', parseFormData, crearInventarioCarnesYFruver);
 
 // Endpoint para obtener los inventarios activos de carnes y fruver
 router.get('/inventarios-carnesYfruver', obtenerInventariosCarnesYFruver);
