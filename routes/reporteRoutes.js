0728c8a2a08719a0f18e0c626f954a8c18cf2c28
isazaaa <isazamanuel04@gmail.com>
2025-09-17 11:09:14 -0500

    Se agrega nuevo controller de analytics para el dashboard ciclico

--- routes/reporteRoutes.js
+++ routes/reporteRoutes.js
@@ -2,13 +2,11 @@ import express from 'express';
 import {
   compararInventario,
   getInventarioDetalle,
-  getDashboardInventarioCiclico 
 } from '../controllers/reporteController.js';
 
 const router = express.Router();
 
 router.get("/comparar-inventario/:id", compararInventario);
 router.get("/TotalInventario", getInventarioDetalle);
-router.get("/dashboard-inventario-ciclico", getDashboardInventarioCiclico);
 
 export default router;
